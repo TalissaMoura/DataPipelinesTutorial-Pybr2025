8038300ee00ebd2005874267a8f2803df7e5bdd4
gabisteph <gabriellepiresmestrinho@gmail.com>
2025-10-22 17:19:00 -0300

    transform pipeline runner in a function

--- coffee_sales_pipeline/src/pipeline_runner.py
+++ coffee_sales_pipeline/src/pipeline_runner.py
@@ -4,7 +4,8 @@ from pipeline.transform import transform_data
 from pipeline.load import load_data
 import os
 
-if __name__ == "__main__":
+def run_pipeline(start_datetime: str, end_datetime: str):
+    """Executa todo o pipeline de vendas de café para um intervalo de data e hora específico."""
     print("☕ Iniciando pipeline de vendas da cafeteria...")
 
     # Definindo caminhos
@@ -14,10 +15,6 @@ if __name__ == "__main__":
     filtered_output_path = os.path.join(DATA_DIR, "filter", "coffee_sales_filter.csv")
     processed_output_path = os.path.join(DATA_DIR, "processed", "coffee_sales_summary.csv")
 
-    # Intervalo desejado
-    start_datetime = "2024-03-01 19:00:00"
-    end_datetime   = "2024-03-01 19:59:00"
-
     # Etapas do pipeline
     df_raw = extract_data(raw_path, start_datetime, end_datetime, filtered_output_path)
     df_valid = validate_data(DATA_DIR, df_raw)  # Usa Great Expectations
@@ -25,3 +22,7 @@ if __name__ == "__main__":
     load_data(df_transformed, processed_output_path)
 
     print("✅ Pipeline finalizado com sucesso!")
+
+# Exemplo de execução
+if __name__ == "__main__":
+    run_pipeline(start_datetime="2024-03-01 19:00:00", end_datetime="2024-03-01 19:59:00")
