b88d8bb270dd57d771dc0b2966b99e949d33f7b4
TalissaMoura <talissa.ayres@gmail.com>
2025-10-22 22:54:22 -0300

    fix: add empty dataframe handler

--- coffee_sales_pipeline/src/pipeline/load.py
+++ coffee_sales_pipeline/src/pipeline/load.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 def load_data(df: pd.DataFrame, output_path: str):
     """Salva o resultado processado."""
-    print("Salvando dados processados em:", output_path)
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    df.to_csv(output_path, index=False)
-    print("Dados salvos com sucesso!")
+    if df.empty:
+        print("DataFrame vazio recebido para salvamento.")
+        return False
+    else:
+        print("Salvando dados processados em:", output_path)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        df.to_csv(output_path, index=False)
+        print("Dados salvos com sucesso!")
+        return True
